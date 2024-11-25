6aa578f3f60326c2fc6d171926c71034e8c6a262
Jean <jeancarlos_bh@outlook.com>
2024-11-25 18:13:09 -0300

    Correção de connections e controller

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2/promise')
+require('dotenv').config();
 
 const connection = mysql.createPool({
     host: process.env.MYSQL_HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB,
 });
 
