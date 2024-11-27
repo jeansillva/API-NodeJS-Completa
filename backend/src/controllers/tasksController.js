da61bec0d953ae878a71652be9883ec7375e3e28
Jean <jeancarlos_bh@outlook.com>
2024-11-27 12:28:49 -0300

    Rota UPDATE & middleware

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,3 +1,4 @@
+const connection = require('../models/connection');
 const tasksModel = require('../models/tasksModel')
 
 const getAll = async (request, response) => {
@@ -17,8 +18,17 @@ const deleteTask = async (request, response) => {
     return response.status(204).json();
 }
 
+const updateTask = async (request, response) => {
+    const { id } = request.params;
+    await tasksModel.updateTask(id, request.body);
+
+    return response.status(204).json();
+}
+
+
 module.exports = {
     getAll,
     createTask,
     deleteTask,
+    updateTask,
 }
--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -20,10 +20,19 @@ const createTask = async (task) => {
 const deleteTask = async (id) => {
     const removedTask = await connection.execute('DELETE FROM tasks WHERE id = ?', [id])
     return removedTask;
-}   
+}  
+
+const updateTask = async (id, task) => {
+    const{ title, status } = task;
+    const query = 'UPDATE tasks SET title = ?, status = ? WHERE id = ?'
+
+    const [updatedTask] = await connection.execute(query, [title, status, id])
+    return updatedTask;
+}
 
 module.exports = {
     getAll,
     createTask,
     deleteTask,
+    updateTask
 }
