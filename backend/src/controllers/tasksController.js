f69b52edf12b65515dca9b11c57b75248d27d90f
Jean <jeancarlos_bh@outlook.com>
2024-11-27 12:10:20 -0300

    Post/delete e middleware de validação

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -7,11 +7,18 @@ const getAll = async (request, response) => {
 
 const createTask = async (request, response) => {
     const createdTask = await tasksModel.createTask(request.body);
-    
+
     return response.status(201).json(createdTask)
+}
+
+const deleteTask = async (request, response) => {
+    const { id } = request.params;
+    await tasksModel.deleteTask(id);
+    return response.status(204).json();
 }
 
 module.exports = {
     getAll,
-    createTask
+    createTask,
+    deleteTask,
 }
